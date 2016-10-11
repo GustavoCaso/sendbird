3f816c39e384376bb3601a0a2f0085cd9e1b269f
GustavoCaso <gustavocaso@gmail.com>
2016-10-11 08:08:34 +0200

    Added Open Channel interface

--- lib/sendbird_api.rb
+++ lib/sendbird_api.rb
@@ -2,6 +2,7 @@ require "sendbird_api/client"
 require "sendbird_api/configuration"
 require "sendbird_api/response"
 require "sendbird_api/user"
+require "sendbird_api/open_channel"
 require "sendbird_api/version"
 
 module SendbirdApi
