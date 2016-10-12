2462453052667385bf2b47ea54a7772794739957
GustavoCaso <gustavocaso@gmail.com>
2016-10-12 13:02:40 +0200

    Added MetaData and MetaCounter interface

--- lib/sendbird_api.rb
+++ lib/sendbird_api.rb
@@ -5,6 +5,8 @@ require "sendbird_api/user"
 require "sendbird_api/open_channel"
 require "sendbird_api/group_channel"
 require "sendbird_api/message"
+require "sendbird_api/meta_data"
+require "sendbird_api/meta_counter"
 require "sendbird_api/version"
 
 module SendbirdApi
