5bcc02a4aab3dffe82f83c77f95d26dc80e93914
GustavoCaso <gustavocaso@gmail.com>
2016-10-12 12:06:09 +0200

    Added Messages interface

--- lib/sendbird_api.rb
+++ lib/sendbird_api.rb
@@ -4,6 +4,7 @@ require "sendbird_api/response"
 require "sendbird_api/user"
 require "sendbird_api/open_channel"
 require "sendbird_api/group_channel"
+require "sendbird_api/message"
 require "sendbird_api/version"
 
 module SendbirdApi
