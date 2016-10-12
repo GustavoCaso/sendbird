d035eb0be7cfd197e5d45d3d843458f7faa72c0a
GustavoCaso <gustavocaso@gmail.com>
2016-10-12 13:08:39 +0200

    Remove more code duplication from MetaBase

--- lib/sendbird_api/meta_base.rb
+++ lib/sendbird_api/meta_base.rb
@@ -3,32 +3,36 @@ module SendbirdApi
     extend Client
     class << self
       def create(channel_type, channel_url, body)
-        post(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT')), body: body)
+        post(path: build_url(channel_type, channel_url, endpoint), body: body)
       end
 
       def view(channel_type, channel_url, params={})
-        get(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT')), params: params)
+        get(path: build_url(channel_type, channel_url, endpoint), params: params)
       end
 
       def view_by_key(channel_type, channel_url, key)
-        get(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT'), key))
+        get(path: build_url(channel_type, channel_url, endpoint, key))
       end
 
       def update(channel_type, channel_url, body)
-        put(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT')), body: body)
+        put(path: build_url(channel_type, channel_url, endpoint), body: body)
       end
 
       def update_by_key(channel_type, channel_url, key, body)
-        put(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT'), key), body: body)
+        put(path: build_url(channel_type, channel_url, endpoint, key), body: body)
       end
 
       def destroy(channel_type, channel_url)
-        delete(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT')))
+        delete(path: build_url(channel_type, channel_url, endpoint))
       end
 
       def destroy_by_key(channel_type, channel_url, key)
-        delete(path: build_url(channel_type, channel_url, self.const_get('ENDPOINT'), key))
+        delete(path: build_url(channel_type, channel_url, endpoint, key))
       end
+    end
+
+    def self.endpoint
+      self.const_get('ENDPOINT')
     end
 
     def self.build_url(*args)
