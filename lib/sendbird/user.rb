120e5b766a2667cd9d0fb7f7fad7b133a75a7da9
GustavoCaso <gustavocaso@gmail.com>
2016-10-19 11:16:15 +0200

    Improve User Interface

--- lib/sendbird/user.rb
+++ lib/sendbird/user.rb
@@ -2,9 +2,11 @@ module Sendbird
   class User
     class InvalidRequest < StandardError; end
 
-    attr_reader :user_id, :pending_requests
+    attr_reader :user_id, :pending_requests, :gcm_tokens, :apns_tokens
     def initialize(user_id)
       @user_id = user_id
+      @gcm_tokens = []
+      @apns_tokens = []
       @pending_requests = []
     end
 
@@ -12,37 +14,217 @@ module Sendbird
       pending_requests.empty?
     end
 
+    #Getters
+    def get_user
+      response = UserApi.show(user_id)
+      if response.status == 200
+        response.body
+      else
+        raise InvalidRequest.new(
+          error_message(
+            response.error_message,
+            __method__,
+            user_id
+          )
+        )
+      end
+    end
+
+    def get_unread_count
+      response = UserApi.unread_count(user_id)
+      if response.status == 200
+        response.body['unread_count']
+      else
+        raise InvalidRequest.new(
+          error_message(
+            response.error_message,
+            __method__,
+            user_id
+          )
+        )
+      end
+    end
+
+    def get_push_preferences
+      response = UserApi.push_preferences(user_id)
+      if response.status == 200
+        response.body
+      else
+        raise InvalidRequest.new(
+          error_message(
+            response.error_message,
+            __method__,
+            user_id
+          )
+        )
+      end
+    end
+
+    # Accessors
+    def user_information=(user_information={})
+      pending_requests << {
+        method: :update,
+        args: [user_id, user_information]
+      }
+      self
+    end
+
     def nickname=(nickname)
-      pending_requests << {method: :update, args: [user_id, {nickname: nickname}]}
+      pending_requests << {
+        method: :update,
+        args: [user_id, {nickname: nickname}]
+      }
       self
     end
 
     def profile_url=(profile_url)
-      pending_requests << {method: :update, args: [user_id, {profile_url: profile_url}]}
+      pending_requests << {
+        method: :update,
+        args: [user_id, {profile_url: profile_url}]
+      }
       self
     end
 
     def issue_access_token=(issue_access_token)
-      pending_requests << {method: :update, args: [user_id, {issue_access_token: issue_access_token}]}
+      pending_requests << {
+        method: :update,
+        args: [user_id, {issue_access_token: issue_access_token}]
+      }
       self
     end
 
     def activate
-      pending_requests << {method: :activate, args: [user_id, { activate: true }]}
+      pending_requests << {
+        method: :activate,
+        args: [user_id, { activate: true }]
+      }
       self
     end
 
     def deactivate
-      pending_requests << {method: :activate, args: [user_id, { activate: false }]}
+      pending_requests << {
+        method: :activate,
+        args: [user_id, { activate: false }]
+      }
       self
     end
 
-    def update!
+    def push_preferences=(push_preferences={})
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, push_preferences]
+      }
+      self
+    end
+
+    def timezone=(timezone)
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, { timezone: timezone }]
+      }
+      self
+    end
+
+    def start_hour=(start_hour)
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, { start_hour: start_hour }]
+      }
+      self
+    end
+
+    def end_hour=(end_hour)
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, { end_hour: end_hour }]
+      }
+      self
+    end
+
+    def start_min=(start_min)
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, { start_min: start_min }]
+      }
+      self
+    end
+
+    def end_min=(end_min)
+      pending_requests << {
+        method: :update_push_preferences,
+        args: [user_id, { end_min: end_min }]
+      }
+      self
+    end
+
+    def register_gcm_token(token)
+      pending_requests << {
+        method: :register_gcm_token,
+        args: [user_id, token],
+        callback: ->(response){ self.gcm_tokens << response.body['token'] }
+      }
+      self
+    end
+
+    def register_apns_token(token)
+      pending_requests << {
+        method: :register_apns_token,
+        args: [user_id, token],
+        callback: ->(response){ self.apns_tokens << response.body['token'] }
+      }
+      self
+    end
+
+    def unregister_gcm_token(token)
+      pending_requests << {
+        method: :unregister_gcm_token,
+        args: [user_id, token],
+        callback: ->(response){ self.gcm_tokens.delete(response.body['token'])}
+      }
+      self
+    end
+
+    def unregister_apns_token(token)
+      pending_requests << {
+        method: :unregister_apns_token,
+        args: [user_id, token],
+        callback: ->(response){ self.apns_tokens.delete(response.body['token'])}
+      }
+      self
+    end
+
+    def unregister_all_device_token
+      pending_requests << {
+        method: :unregister_all_device_token,
+        args: [user_id],
+        callback: ->(response) { self.apns_tokens = []; self.gcm_tokens = []; }
+      }
+      self
+    end
+
+    def mark_as_read_all
+      pending_requests << {
+        method: :mark_as_read_all,
+        args: [user_id]
+      }
+      self
+    end
+
+    def request!
       pending_requests.each do |request|
         response = UserApi.send(request[:method], *request[:args])
         if response.status != 200
           self.pending_requests = []
-          raise InvalidRequest, "Invalid request for User with user_id: #{user_id}, error message: #{response.error_message}, the pending requets will be clear"
+          raise InvalidRequest.new(
+            error_message(
+              response.error_message,
+              request[:method],
+              request[:args]
+            )
+          )
+        end
+        if request[:callback]
+          request[:callback].call(response)
         end
       end
       self.pending_requests = []
@@ -50,66 +232,10 @@ module Sendbird
     end
 
     private
-    attr_writer :pending_requests
+    attr_writer :pending_requests, :apns_tokens, :gcm_tokens
 
-    def push_preferences
-      UserApi.push_preferences(user_id).body
-    end
-
-    def update_push_preferences(body)
-      UserApi.update_push_preferences(user_id, body)
-    end
-
-    def delete_push_preferences
-      UserApi.delete_push_preferences(user_id)
-    end
-
-    def update_timezone(timezone)
-      UserApi.update_push_preferences(user_id, { timezone: timezone }).body
-    end
-
-    def update_start_hour(start_hour)
-      UserApi.update_push_preferences(user_id, { start_hour: start_hour }).body
-    end
-
-    def update_end_hour(end_hour)
-      UserApi.update_push_preferences(user_id, { end_hour: end_hour }).body
-    end
-
-    def update_start_min(start_min)
-      UserApi.update_push_preferences(user_id, { start_min: start_min }).body
-    end
-
-    def update_end_min(end_min)
-      UserApi.update_push_preferences(user_id, { end_min: end_min }).body
-    end
-
-    def unread_count
-      UserApi.unread_count(user_id).body['unread_count']
-    end
-
-    def mark_as_read_all
-      UserApi.mark_as_read_all(user_id)
-    end
-
-    def register_gcm_token(token)
-      UserApi.register_gcm_token(user_id, token).body
-    end
-
-    def register_apns_token(token)
-      UserApi.register_apns_token(user_id, token).body
-    end
-
-    def unregister_gcm_token(token)
-      UserApi.unregister_gcm_token(user_id, token).body
-    end
-
-    def unregister_apns_token(token)
-      UserApi.unregister_apns_token(user_id, token).body
-    end
-
-    def unregister_all_device_token
-      UserApi.unregister_all_device_token(user_id).body
+    def error_message(error_message, method_name, args)
+      "Invalid request for User with user_id: #{user_id}, error message: #{error_message} the request method was #{method_name} with args: #{args}"
     end
   end
 end
--- spec/sendbird/user_spec.rb
+++ spec/sendbird/user_spec.rb
@@ -5,6 +5,19 @@ describe Sendbird::User do
     described_class.new('testing_user_interface_1')
   end
 
+  context 'Getters' do
+    let(:request) do
+      create_dynamic_cassette("#{described_class}/get_user") do
+        subject.get_user
+      end
+    end
+
+    it '#get_user' do
+      expect(request).to be_a Hash
+      expect(request.keys).to include('user_id', 'nickname', 'profile_url', 'access_token', 'last_seen_at', 'is_online')
+    end
+  end
+
   context 'Pending Request' do
     it 'will add a new pending request, with the correct structure' do
       subject.nickname = 'Mckuly'
@@ -14,10 +27,10 @@ describe Sendbird::User do
     end
   end
 
-  context '#update!' do
+  context '#request!' do
     let(:request) do
-      create_dynamic_cassette("#{described_class}/update!") do
-        subject.update!
+      create_dynamic_cassette("#{described_class}/request!") do
+        subject.request!
       end
     end
     it 'will clear pending_requests after finishing' do
@@ -32,13 +45,47 @@ describe Sendbird::User do
       expect(subject.in_sync?).to be_truthy
     end
 
+    context 'With callback' do
+      subject do
+        described_class.new('testing_user_interface_1')
+      end
+
+      let(:register_gcm_token) do
+        create_dynamic_cassette("#{described_class}/register_gcm_token") do
+          subject.request!
+        end
+      end
+
+      let(:unregister_tokens) do
+        create_dynamic_cassette("#{described_class}/unregister_tokens") do
+          subject.request!
+        end
+      end
+
+      it 'will register_gcm_token and call the callback function' do
+        subject.register_gcm_token('dfeufue8hib7g')
+        register_gcm_token
+        expect(subject.gcm_tokens).to include('dfeufue8hib7g')
+      end
+
+      it 'will delete all tokens' do
+        subject.register_gcm_token('dfeufue8hib7g')
+        register_gcm_token
+        expect(subject.gcm_tokens).to_not be_empty
+        subject.unregister_all_device_token
+        unregister_tokens
+        expect(subject.gcm_tokens).to eq([])
+        expect(subject.apns_tokens).to eq([])
+      end
+    end
+
     context 'Error on request' do
       subject do
         described_class.new('non_existing')
       end
       let(:request) do
         create_dynamic_cassette("#{described_class}/invalid_request") do
-          subject.update!
+          subject.request!
         end
       end
       it 'will raise an Error' do
